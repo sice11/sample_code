532d91f4f886579c3d5ff738b3b05d0626177fa0
yone_tomo <yonezawa_t@cfk.co.jp>
2021-02-10 10:55:07 +0900

    Max_Path_File_Delete_making

--- sample.py
+++ sample.py
@@ -1,5 +1,19 @@
 import glob
+import os
 
-matchPath = glob.glob("**/sample.txt", recursive=True)
+file_list = [
+    "a.txt",
+    "b.txt",
+    "c.txt",
+]
 
-print(matchPath)
+
+for file in file_list:
+    print(file)
+    matchPath = glob.glob("**/{}".format(file), recursive=True)
+    # matchPathはリストで取得される
+    print(matchPath[0])
+
+    os.remove(file)
+
+    # 作成途中 どうやら、MAX_PATHを超えているから、エラーが出る。
