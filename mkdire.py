de2d0d75877f9da9113571c76acee9254b8b8d06
yone_tomo <yonezawa_t@cfk.co.jp>
2021-02-17 16:48:32 +0900

    make_file_list add

--- mkdire.py
+++ mkdire.py
@@ -3,9 +3,17 @@ import pathlib
 
 new_dir_path = "./sample1/sample2/"
 
-filename = "hoge.txt"
+# 作成するファイル一覧
+make_files = [
+    "a.txt",
+    "b.txt",
+    "c.txt",
+]
 
+# 新規でフォルダ階層を作成
 os.makedirs(new_dir_path)
 
-f = pathlib.Path(new_dir_path + "sample.txt")
-f.touch()
+# 新規で作成したディレクトリ最下部に対象のファイルを配置
+for make_file in make_files:
+    f = pathlib.Path(new_dir_path + make_file)
+    f.touch()
